061494de7cfc96cbab9c36cd088d7fd6f6154e48
Dario Alvarez <dario@electronic-bits.com>
2019-03-17 22:27:12 +1100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var world = 'world';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world + "! ";
+}
+console.log(hello());
+//# sourceMappingURL=index.js.map
